6a90366786b1d7a2b58e9ca97981b8158339bcbd
Sharon Cherop <sh.cherop@gmail.com>
2021-07-25 22:46:33 +0300

    Edited portfolio images with hovering effect

--- js/script.js
+++ js/script.js
@@ -31,6 +31,61 @@ $(document).ready(function() {
         $(".p3txt").toggle();
         $("#p-main3").css('background', 'none')
     });
+
+    $(".p-main4").mouseenter(function(){
+        $(".img-hide4").toggle();
+        $(".p4txt").toggle();
+        $('#p-main4').css('background','url(./assets/portfolio/work1.jpg)')
+    });
+    $(".p-main4").mouseleave(function() {
+        $(".img-hide4").toggle();
+        $(".p4txt").toggle();
+        $("#p-main4").css('background', 'none')
+    });
+
+    $(".p-main5").mouseenter(function(){
+        $(".img-hide5").toggle();
+        $(".p5txt").toggle();
+        $('#p-main5').css('background','url(./assets/portfolio/work5.jpg)')
+    });
+    $(".p-main5").mouseleave(function() {
+        $(".img-hide5").toggle();
+        $(".p5txt").toggle();
+        $("#p-main5").css('background', 'none')
+    });
+
+    $(".p-main6").mouseenter(function(){
+        $(".img-hide6").toggle();
+        $(".p6txt").toggle();
+        $('#p-main6').css('background','url(./assets/portfolio/work6.jpg)')
+    });
+    $(".p-main6").mouseleave(function() {
+        $(".img-hide6").toggle();
+        $(".p6txt").toggle();
+        $("#p-main6").css('background', 'none')
+    });
+
+    $(".p-main7").mouseenter(function(){
+        $(".img-hide7").toggle();
+        $(".p7txt").toggle();
+        $('#p-main7').css('background','url(./assets/portfolio/work7.jpg)')
+    });
+    $(".p-main7").mouseleave(function() {
+        $(".img-hide7").toggle();
+        $(".p7txt").toggle();
+        $("#p-main7").css('background', 'none')
+    });
+
+    $(".p-main8").mouseenter(function(){
+        $(".img-hide8").toggle();
+        $(".p8txt").toggle();
+        $('#p-main8').css('background','url(./assets/portfolio/work8.jpg)')
+    });
+    $(".p-main8").mouseleave(function() {
+        $(".img-hide8").toggle();
+        $(".p8txt").toggle();
+        $("#p-main8").css('background', 'none')
+    });
 });
 
 
